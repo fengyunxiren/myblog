78b244f1107ef8c9486a88e4a9c347905fb0b234
wangyc <fengyunxiren@163.com>
2018-03-28 16:50:33 +0800

    add article detail

--- blogfront/src/components/ArticleDetail.vue
+++ blogfront/src/components/ArticleDetail.vue
@@ -1,9 +1,28 @@
 <template>
-    <div>
-        <p>{{ msg }}</p>
-        <p>{{ $route.params.id }}</p>
-        <p>{{article.content}}</p>
+  <div>
+    <div class="detail-info">
+      <p class="detail-title">{{ article.title }}</p>
+      <p class="detail-author">
+          <span>
+              <i class="el-icon-edit-outline"></i>
+              {{ article.author }}
+          </span>
+          <span>
+              <i class="el-icon-time"></i>
+              {{ transDate(article.update_time) }}
+          </span>
+      </p>
     </div>
+    <mavon-editor class="detail-editor"
+      v-model="article.content"
+      :defaultOpen="preview"
+      :subfield="false"
+      :editable="false"
+      :toolbarsFlag="false"
+      :ishljs="true"
+      border-color="#fff"
+      ></mavon-editor>
+  </div>
 </template>
 
 <script>
@@ -32,7 +51,35 @@ export default {
             console.log(res.message)
           }
         })
+    },
+    transDate (timeString) {
+      var newString = ''
+      newString = timeString.replace(/T/, ' ')
+      newString = newString.split('.')[0]
+      return newString
     }
   }
 }
 </script>
+
+<style>
+  .detail-info {
+    background-color: #fff;
+  }
+  .detail-title {
+    margin-top: 20px;
+    margin-bottom: 0px;
+    color: #000;
+    font-weight: 600;
+    font-size: 60px;
+    cursor: pointer;
+  }
+  .detail-author {
+    margin-top: 15px;
+    margin-bottom: 0px;
+    font-size: 12px;
+    color: #372d30;
+    text-align: left;
+    text-indent: 6px;
+  }
+</style>
--- blogfront/src/main.js
+++ blogfront/src/main.js
@@ -6,10 +6,13 @@ import router from './router'
 import ElementUI from 'element-ui'
 import VueResource from 'vue-resource'
 import 'element-ui/lib/theme-chalk/index.css'
+import mavonEditor from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 Vue.use(VueResource)
+Vue.use(mavonEditor)
 
 /* eslint-disable no-new */
 new Vue({
